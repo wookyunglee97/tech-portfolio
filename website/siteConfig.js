e01974c60e7522968864c39a072ba7091b53e18d
wookyunglee97 <wl357@cornell.edu>
2020-10-02 15:20:42 -0400

    Updated tagline

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,7 +13,7 @@ const users = [];
 
 const siteConfig = {
     title: 'Chloe Lee', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    tagline: 'Aspiring PM with a passion for learning new technologies',
     profilePhoto: 'img/profile.png',
     url: 'https://wookyunglee97.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
